ea0801d803127ff0d1b0e0558df27d520764b53b
Anan Paenthongkham <hs1gab@gmail.com>
2018-06-04 14:18:38 +0700

    test 003

---
+++ mongDB.js
@@ -0,0 +1,45 @@
+var MongoClient = require('mongodb').MongoClient;
+var config = require('./config.json');
+var url = "mongodb://" + config.mongoDB.user + ":" + config.mongoDB.password + "@localhost:27017/";
+
+var obj = {} ;
+obj.finduser =  function ( quser ){
+         var txt = "old value" ;
+  MongoClient.connect(url, function(err, db) {
+         var x = "" ;
+	 if (err) throw err;
+	 var dbo = db.db("accounts");
+	 var query = { userlogin : quser  };
+         let respdb ;
+         dbo.collection("users").find(query).toArray(function(err, result ) {
+	       if (err) throw err;
+	       txt = "<table>" ;
+	       for (x in result ) {
+			txt += "<tr><td>" + result[x].IDcardpass +
+			    "</td><td>" + result[x].userlogin +
+			    "</td><td>" + result[x].firstname +
+			    "</td><td>" + result[x].lastname +
+			    "</td><td>" + result[x].email + "</td></tr>\n";
+		    }
+	       txt += "</table>" ;
+	       db.close();
+               console.log( "new value txt>>>" + txt ) ;
+	  });
+               return txt ; 
+  }); //END MongoClient.connect
+} // END finduser 
+obj.updateuser = function ( uUser ) {
+  return " Update user :" + uUser ;
+};
+obj.deleteuser = function ( uUser ) {
+  return " Delete user :" + uUser ;
+};  
+
+var hello = function( hUser ){
+  return "Hello " + hUser  ;
+}
+
+exports.data=obj ; // Method
+exports.hello=hello ; // Var 
+//exports.finduser=finduser ; // Var 
+
